a8daed9776604f060b13d18fe3728d3fc4698c92
FrontendTopG <aleenfestus@gmail.com>
2025-04-27 13:09:25 +0100

    tiny adjustments

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,38 +5,37 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col">
       {/* Hero section */}
-      <div className="w-full flex flex-col lg:flex-row justify-center items-center md:px-[5%] px-6 max-w-9xl mx-auto md:min-h-[calc(100vh-96px)] min-h-[calc(100vh-80px)] overflow-hidden lg:gap-20 gap-14">
+      <div className="w-full flex flex-col lg:flex-row justify-center items-center md:px-[5%] px-6 max-w-9xl mx-auto md:min-h-[calc(100vh-96px)] min-h-[calc(100vh-80px)] overflow-hidden lg:gap-20 gap-14 lg:pt-0 pt-24">
         <div className="flex flex-col justify-start lg:items-start items-center w-full max-w-xl mt-12 md:mt-0 lg:text-left text-center gap-4">
           <Animate
             as="h1"
-            className="font-crimson font-medium lg:text-6xl md:text-5xl text-[40px] leading-[1]"
+            className="font-crimson font-medium md:text-6xl xs:text-5xl text-[40px] leading-[1]"
           >
             Unlock Deep <br /> Transformation <br /> & Scale Outcomes
           </Animate>
           <Animate
             as="p"
-            delay={75}
-            className="lg:text-2xl md:text-lg text-lg font-normal text-brand-grey-2"
+            delay={100}
+            className="md:text-2xl text-lg font-normal text-brand-grey-2"
           >
             Empowering high-growth individuals to unlock their highest potential
             through high-performance strategies.
           </Animate>
 
-          <Animate delay={150}>
-            <Button className="mt-6 px-8 text-base">Work with Eduwa</Button>
+          <Animate delay={200}>
+            <Button className="mt-6 px-6 text-base">Work with Eduwa</Button>
           </Animate>
         </div>
 
         <Animate
-          delay={225}
-          className="overflow-hidden sm:min-w-[460px] sm:min-h-[530px] aspect-[560/720] w-fit h-fit mt-auto"
+          delay={350}
+          className="overflow-hidden lg:min-w-[560px] sm:min-w-[460px] sm:min-h-[530px] aspect-[560/720] w-fit h-fit mt-auto"
         >
           <img
             width={560}
             height={720}
             loading="eager"
             src="/images/eduwa.png"
-            alt="Eduwa Osa"
             className="h-full w-full object-center object-contain relative"
           />
         </Animate>
